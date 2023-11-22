feeb2ab85c349f8f4b3df6707e2494cd854064b9
Redwan islam <redwanislam2760@gmail.com>
2023-11-22 22:32:01 +0600

    complete defined user validation schema

---
+++ src/app/module/users/user.validation.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+// Define the Zod schema
+const addressSchema = z.object({
+  street: z.string().min(1).max(30),
+  city: z.string().min(1),
+});
+
+const fullNameSchema = z.object({
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+});
+
+const userZodSchema = z.object({
+  username: z.string().min(2).max(30),
+  fullName: fullNameSchema,
+  email: z.string().min(3),
+  password: z.string().min(4).max(40),
+  age: z.number(),
+  hobbies: z.array(z.string()),
+  address: addressSchema,
+  isActive: z.boolean(),
+});
+
+export const zodValidationSchema = {
+  userZodSchema,
+};
